6219ee8941eab33f80741d68ac8a56d549a72929
Nathaniel Micklewright <nathaniel.john.m@gmail.com>
2022-04-23 19:14:40 +0900

    Return everything back to normal

--- Cpp/HelloWorld/Math.cpp
+++ Cpp/HelloWorld/Math.cpp
@@ -1,9 +1,5 @@
-#define INTEGER int
-
-#if 0
-INTEGER Multiply(int a, int b)
+int Multiply(int a, int b)
 {
-  INTEGER result = a * b;
+  int result = a * b;
   return result;
-#include "EndBrace.h"
-#endif
+}
