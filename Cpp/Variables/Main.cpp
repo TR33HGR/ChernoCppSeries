d4633b4b71dd6cdf629f86a5164be2ac5a658de9
Nathaniel Micklewright <nathaniel.john.m@gmail.com>
2022-04-24 15:26:39 +0900

    Unsigned ints exist

--- Cpp/Variables/Main.cpp
+++ Cpp/Variables/Main.cpp
@@ -3,6 +3,7 @@
 int main()
 {
   int variable = 8; // ~ +/-2b
+  unsigned int unsigned_variable = 8; // ~ 4b
   std::cout << variable << std::endl;
   variable = 20;
   std::cout << variable << std::endl;
