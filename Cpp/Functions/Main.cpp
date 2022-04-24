49290f2acbfa853f71430870eb33956d8862c1ce
Nathaniel Micklewright <nathaniel.john.m@gmail.com>
2022-04-24 15:51:08 +0900

    Copy & paste problem, where I have forgotted to update the reult being printed

--- Cpp/Functions/Main.cpp
+++ Cpp/Functions/Main.cpp
@@ -9,5 +9,11 @@ int main()
 {
   int result = Multiply(3, 2);
   std::cout << result << std::endl;
+
+  int result2 = Multiply(4, 2);
+  std::cout << result << std::endl;
+
+  int result3 = Multiply(3, 45);
+  std::cout << result << std::endl;
   std::cin.get();
 }
